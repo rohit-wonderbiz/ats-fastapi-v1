974bd8061fb831e95f16019d809972a28c3948cd
Rohit Wahwal <rohit.wahwal@wonderbiz.in>
2024-08-14 09:49:56 +0530

    Add config.py example

---
+++ config.example.py
@@ -0,0 +1,10 @@
+waitTime = 10
+cameraType="OUT"
+server = ""
+port = ""
+user = ''
+password = ""
+database = ""
+videoSource = 0
+
+connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server},{port};DATABASE={database};UID={user};PWD={password}'
